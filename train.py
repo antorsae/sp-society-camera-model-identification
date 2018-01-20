bd6a2c7349645044cc88cdae59d67d34ea3b8626
Andres Torrubia <antorsae@gmail.com>
2018-01-20 13:59:24 +0100

    uses 512x512 centered crops now, WiP using Sequence-based generator instead of plain gen, fixed preprocessed file name when using -kf

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, LambdaCallback
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model, Model
 from keras.layers import concatenate, Lambda, Input, Dense, Dropout, Flatten
-from keras.utils import to_categorical
+from keras.utils import to_categorical, Sequence
 from keras.initializers import Constant
 from keras.applications import *
 
@@ -23,7 +23,7 @@ from multi_gpu_keras import multi_gpu_model
 from keras import backend as K
 from keras.engine.topology import Layer
 import skimage
-import hickle
+import pickle
 from iterm import show_image
 
 import itertools
@@ -40,21 +40,22 @@ import math
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--max-epoch', type=int, default=200, help='Epoch to run')
-parser.add_argument('-b', '--batch-size', type=int, default=64, help='Batch Size during training, e.g. -b 2')
-parser.add_argument('-s', '--sub-batch-size', type=int, default=4, help='Number of crops from same image for each batch')
-parser.add_argument('-ba', '--batch-acc', type=int, default=1, help='Batch Size for training accumulation, e.g. -b 4')
+parser.add_argument('-b', '--batch-size', type=int, default=64, help='Batch Size during training, e.g. -b 64')
+parser.add_argument('-s', '--sub-batch-size', type=int, default=1, help='Number of crops from same image for each batch')
 parser.add_argument('-l', '--learning_rate', type=float, default=1e-3, help='Initial learning rate')
 parser.add_argument('-m', '--model', help='load hdf5 model (and continue training)')
 parser.add_argument('-t', '--test', action='store_true', help='Test model and generate CSV submission file')
 parser.add_argument('-cs', '--crop-size', type=int, default=299, help='Crop size')
 parser.add_argument('-g', '--gpus', type=int, default=1, help='Number of GPUs to use')
-parser.add_argument('-pp','--preprocessed_input_path', type=str, default='images_preprocessed.hkl', help='Path to preprocessed images')
+parser.add_argument('-pp','--preprocessed_input_path', type=str, default='images_preprocessed', help='Path to preprocessed images')
 parser.add_argument('-p', '--pooling', type=str, default='avg', help='Type of pooling to use')
 parser.add_argument('-kf', '--kernel-filter', action='store_true', help='Apply kernel filter')
 parser.add_argument('-cm', '--classifier', type=str, default='ResNet50', help='Base classifier model to use')
 parser.add_argument('-uiw', '--use-imagenet-weights', action='store_true', help='Use imagenet weights (transfer learning)')
 
 args = parser.parse_args()
+
+args.preprocessed_input_path += '-kf.pkl' if args.kernel_filter else '.pkl'
 
 TRAIN_FOLDER = 'train'
 TEST_FOLDER  = 'test'
@@ -74,6 +75,7 @@ CLASSES = [
   'Sony-NEX-7']
 
 N_CLASSES = len(CLASSES)
+load_img   = lambda img_path: jpeg.JPEG(img_path).decode()
 
 def gen(items, batch_size, training=True, inference=False):
 
@@ -86,14 +88,13 @@ def gen(items, batch_size, training=True, inference=False):
   O = np.zeros((batch_size, 2), dtype=np.float32)
 
   if not inference:
-    y = np.zeros((batch_size), dtype=np.float32)
-
-  load_img   = lambda img_path: jpeg.JPEG(img_path).decode()
+    y = np.zeros((batch_size), dtype=np.int64)
 
   batch_idx = 0
   
-  if os.path.isfile(args.preprocessed_input_path):
-    images_cached = hickle.load(args.preprocessed_input_path)
+  if os.path.isfile(args.preprocessed_input_path) and training:
+    print("Loading preprocessed images from: "+ args.preprocessed_input_path)
+    images_cached = pickle.load(open(args.preprocessed_input_path, "rb")) 
 
   while True:
     if training:
@@ -112,7 +113,9 @@ def gen(items, batch_size, training=True, inference=False):
         img = load_img(item)
 
         # resize image so it's same as test images
-        img = skimage.transform.resize(img, (512, 512), mode='reflect')
+        center_x, center_y = img.shape[1] // 2, img.shape[0] // 2
+        half_crop = 512//2
+        img = img[center_y -half_crop : center_y + half_crop, center_x - half_crop : center_x + half_crop]
 
         if args.kernel_filter:
           # see slide 13
@@ -153,7 +156,6 @@ def gen(items, batch_size, training=True, inference=False):
         images_cached[item] = img
 
       else:
-
         img = images_cached[item]
 
       # to save time use sub_batch_size crops from same image on the same batch
@@ -163,9 +165,9 @@ def gen(items, batch_size, training=True, inference=False):
 
       for sub_batch in range(sub_batch_size):
 
-        # right now just take corners of image as cropes
-        sx= random.randint(0, 1)
-        sy= random.randint(0, 1)
+        # right now just take corners of image as crops
+        sx= 0 #random.randint(0, 1)
+        sy= 0 #random.randint(0, 1)
 
         _sx = img.shape[1] - CROP_SIZE if sx == 1 else 0
         _sy = img.shape[0] - CROP_SIZE if sy == 1 else 0
@@ -196,10 +198,99 @@ def gen(items, batch_size, training=True, inference=False):
 
         batch_idx = 0
 
-    # commented b/c hickle has a bug (apparently): https://github.com/telegraphic/hickle/issues/63
-    #if not os.path.isfile(args.preprocessed_input_path):
-    #  hickle.dump(images_cached, args.preprocessed_input_path, mode='w')
+    if not os.path.isfile(args.preprocessed_input_path) and training:
+      pickle.dump(images_cached, open(args.preprocessed_input_path, 'wb'))
 
+# attempt to use Sequence instead of generator to allow multiprocessing 
+# but still it's substantially slower than generator with cached images
+class CameraImagesSequence(Sequence):
+
+  def __init__(self, items, batch_size, training=True, inference=False):
+    self.X_path = items
+    self.y      = [CLASSES.index(item.split('/')[-2]) for item in items]
+
+    self.batch_size = batch_size
+    self.training   = training
+    self.inference  = inference
+
+  def __len__(self):
+    return math.ceil(len(self.X_path) / self.batch_size)
+
+  def __getitem__(self, idx):
+    batch_X_path = self.X_path[idx * self.batch_size:(idx + 1) * self.batch_size]
+    batch_y      = self.y     [idx * self.batch_size:(idx + 1) * self.batch_size]
+
+    X = np.zeros((self.batch_size, CROP_SIZE, CROP_SIZE, 3), dtype=np.float32)
+
+    # O holds normalized float of where the crop is in the image (local relative position)
+    O = np.zeros((self.batch_size, 2), dtype=np.float32)
+    y = np.zeros((self.batch_size),    dtype=np.int64)
+
+    for it, (item, class_idx) in enumerate(zip(batch_X_path, batch_y)):
+      img = load_img(item)
+
+      # resize image so it's same as test images
+      img = skimage.transform.resize(img, (512, 512), mode='reflect')
+
+      if args.kernel_filter:
+        # see slide 13
+        # http://www.lirmm.fr/~chaumont/publications/WIFS-2016_TUAMA_COMBY_CHAUMONT_Camera_Model_Identification_With_CNN_slides.pdf
+        kernel_filter = 1/12. * np.array([\
+          [-1,  2,  -2,  2, -1],  \
+          [ 2, -6,   8, -6,  2],  \
+          [-2,  8, -12,  8, -2],  \
+          [ 2, -6,   8, -6,  2],  \
+          [-1,  2,  -2,  2, -1]]) 
+
+        img = cv2.filter2D(img.astype(np.float32),-1,kernel_filter)
+        # kernel filter already puts mean ~0 and roughly scales between [-1..1]
+        # no need to preprocess_input further
+      else:
+        # find `preprocess_input` function specific to the classifier
+        classifier_to_module = { 
+          'NASNetLarge'       : 'nasnet',
+          'NASNetMobile'      : 'nasnet',
+          'DenseNet121'       : 'densenet',
+          'DenseNet161'       : 'densenet',
+          'DenseNet201'       : 'densenet',
+          'InceptionResNetV2' : 'inception_resnet_v2',
+          'InceptionV3'       : 'inception_v3',
+          'MobileNet'         : 'mobilenet',
+          'ResNet50'          : 'resnet50',
+          'VGG16'             : 'vgg16',
+          'VGG19'             : 'vgg19',
+          'Xception'          : 'xception',
+        }
+
+        classifier_module_name = classifier_to_module[args.classifier]
+        preprocess_input_function = getattr(globals()[classifier_module_name], 'preprocess_input')
+        img = preprocess_input_function(img.astype(np.float32))
+
+      # at this point we now have img
+      sx= random.randint(0, 1)
+      sy= random.randint(0, 1)
+
+      _sx = img.shape[1] - CROP_SIZE if sx == 1 else 0
+      _sy = img.shape[0] - CROP_SIZE if sy == 1 else 0
+
+      _img = img[_sy:_sy+CROP_SIZE, _sx:_sx+CROP_SIZE]
+
+      X[it] = _img
+      O[it] = np.float32([sx, sy]) - np.float32([0.5,0.5])
+      y[it] = class_idx
+
+    return [X, O], y
+
+  # instead could have used shuffle=True on .fit_generator(...) https://keras.io/models/model/
+  def on_epoch_end(self):
+      """Method called at the end of every epoch.
+      """
+      print('\nEpoch end' )
+      if self.training:
+        print("Shuffling")
+        I_O_zipped = list(zip(self.X_path, self.y))
+        random.shuffle(I_O_zipped)
+        self.X_path, self.y = zip(*I_O_zipped)
 
 # Custom layer used to do filtering on the GPU
 # Not sure if it yields correct results so not using right now.
@@ -266,7 +357,7 @@ else:
 
   model.summary()
 
-  model_name = args.classifier
+  model_name = args.classifier + ('_kf' if args.kernel_filter else '')
 
 model = multi_gpu_model(model, gpus=args.gpus)
 
@@ -298,4 +389,7 @@ if True:
       validation_steps = int(math.ceil(len(ids_val) // args.batch_size)),
       epochs = args.max_epoch,
       callbacks = [save_checkpoint, reduce_lr],
-      initial_epoch = last_epoch)
+      initial_epoch = last_epoch, )
+      #use_multiprocessing=True, 
+      #workers=31,
+      #max_queue_size=16)
