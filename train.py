3427c7f26fce63079de89db7877fec3d68345678
Andres Torrubia <antorsae@gmail.com>
2018-01-22 18:16:22 +0100

    all manipulations incl. bicubic resizing, -tt to test model

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from keras.losses import binary_crossentropy
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, LambdaCallback
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model, Model
-from keras.layers import concatenate, Lambda, Input, Dense, Dropout, Flatten
+from keras.layers import concatenate, Lambda, Input, Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Activation
 from keras.utils import to_categorical, Sequence
 from keras.initializers import Constant
 from keras.applications import *
@@ -40,6 +40,11 @@ import cv2
 import math
 import csv
 
+SEED = 42
+
+np.random.seed(SEED)
+random.seed(SEED)
+# TODO tf seed
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--max-epoch', type=int, default=100, help='Epoch to run')
@@ -49,7 +54,8 @@ parser.add_argument('-l', '--learning_rate', type=float, default=1e-3, help='Ini
 parser.add_argument('-m', '--model', help='load hdf5 model (and continue training)')
 parser.add_argument('-do', '--dropout', type=float, default=0.3, help='Dropout rate')
 parser.add_argument('-t', '--test', action='store_true', help='Test model and generate CSV submission file')
-parser.add_argument('-cs', '--crop-size', type=int, default=299, help='Crop size')
+parser.add_argument('-tt', '--test-train', action='store_true', help='Test model on the training set')
+parser.add_argument('-cs', '--crop-size', type=int, default=512, help='Crop size')
 parser.add_argument('-g', '--gpus', type=int, default=1, help='Number of GPUs to use')
 parser.add_argument('-pp','--preprocessed_input_path', type=str, default='images_preprocessed', help='Path to preprocessed images')
 parser.add_argument('-p', '--pooling', type=str, default='avg', help='Type of pooling to use')
@@ -59,7 +65,7 @@ parser.add_argument('-uiw', '--use-imagenet-weights', action='store_true', help=
 
 args = parser.parse_args()
 
-args.preprocessed_input_path += '-kf.pkl' if args.kernel_filter else '.pkl'
+args.preprocessed_input_path += '.pkl'
 
 TRAIN_FOLDER = 'train'
 TEST_FOLDER  = 'test'
@@ -78,10 +84,11 @@ CLASSES = [
     'Samsung-Galaxy-Note3',
     'Sony-NEX-7']
 
-MANIPULATIONS = ['jpg70', 'jpg90', 'gamma0.8', 'gamma1.2']
+MANIPULATIONS = ['jpg70', 'jpg90', 'gamma0.8', 'gamma1.2', 'bicubic0.5', 'bicubic0.8', 'bicubic1.5', 'bicubic2.0']
 
 N_CLASSES = len(CLASSES)
-load_img  = lambda img_path: jpeg.JPEG(img_path).decode()
+#load_img  = lambda img_path: jpeg.JPEG(img_path).decode()
+load_img  = lambda img_path: np.array(Image.open(img_path))
 
 def random_manipulation(img, manipulation=None):
 
@@ -89,20 +96,21 @@ def random_manipulation(img, manipulation=None):
         manipulation = random.choice(MANIPULATIONS)
 
     if manipulation.startswith('jpg'):
+        quality = int(manipulation[3:])
         out = BytesIO()
         im = Image.fromarray(img)
-        im.save(out, format='jpeg', quality=70 if manipulation=='jpeg70' else 90)
+        im.save(out, format='jpeg', quality=quality)
         im_decoded = jpeg.JPEG(np.frombuffer(out.getvalue(), dtype=np.uint8)).decode()
         del out
         del im
     elif manipulation.startswith('gamma'):
-        gamma = 0.8 if manipulation == 'gamma0.8' else 1.2
+        gamma = float(manipulation[5:])
         # alternatively use skimage.exposure.adjust_gamma
         # img = skimage.exposure.adjust_gamma(img, gamma)
-        img = img/255.0
-        img = cv2.pow(img, gamma)
-        im_decoded = np.uint8(img*255)
-
+        im_decoded = np.uint8(cv2.pow(img / 255., gamma)*255.)
+    elif manipulation.startswith('bicubic'):
+        scale = float(manipulation[7:])
+        im_decoded = cv2.resize(img,(0,0), fx=scale, fy=scale, interpolation = cv2.INTER_CUBIC)
     return im_decoded
 
 def preprocess_image(img):
@@ -117,10 +125,9 @@ def preprocess_image(img):
             [ 2, -6,   8, -6,  2],  \
             [-1,  2,  -2,  2, -1]]) 
 
-        img = cv2.filter2D(img.astype(np.float32),-1,kernel_filter)
+        return cv2.filter2D(img.astype(np.float32),-1,kernel_filter)
         # kernel filter already puts mean ~0 and roughly scales between [-1..1]
         # no need to preprocess_input further
-        return img
     else:
         # find `preprocess_input` function specific to the classifier
         classifier_to_module = { 
@@ -136,9 +143,14 @@ def preprocess_image(img):
             'VGG16'             : 'vgg16',
             'VGG19'             : 'vgg19',
             'Xception'          : 'xception',
+
         }
 
-        classifier_module_name = classifier_to_module[args.classifier]
+        if args.classifier in classifier_to_module:
+            classifier_module_name = classifier_to_module[args.classifier]
+        else:
+            classifier_module_name = 'xception'
+
         preprocess_input_function = getattr(globals()[classifier_module_name], 'preprocess_input')
         return preprocess_input_function(img.astype(np.float32))
 
@@ -162,9 +174,21 @@ def get_size(obj, seen=None):
                 size += sum([get_size(i, seen) for i in obj])
         return size
 
+def get_crop(img, crop_size):
+    # resize image so it's same as test images and pad it (reflect) if requested crop is bigger than image
+    center_x, center_y = img.shape[1] // 2, img.shape[0] // 2
+    half_crop = crop_size // 2
+    pad_x = max(0, crop_size - img.shape[1])
+    pad_y = max(0, crop_size - img.shape[0])
+    if (pad_x > 0) or (pad_y > 0):
+        img = np.pad(img, ((pad_y//2, pad_y - pad_y//2), (pad_x//2, pad_x - pad_x//2), (0,0)), mode='reflect')
+        center_x, center_y = img.shape[1] // 2, img.shape[0] // 2
+    return img[center_y - half_crop : center_y + half_crop, center_x - half_crop : center_x + half_crop]
+
+
 def gen(items, batch_size, training=True, inference=False):
 
-    images_cached = {}
+    images_cached = { }
 
     # X holds image crops
     X = np.zeros((batch_size, CROP_SIZE, CROP_SIZE, 3), dtype=np.float32)
@@ -197,51 +221,35 @@ def gen(items, batch_size, training=True, inference=False):
             if item not in images_cached:
 
                 img = load_img(item)
-
-                # resize image so it's same as test images
-                center_x, center_y = img.shape[1] // 2, img.shape[0] // 2
-                half_crop = 512//2
-                img = np.array(img[center_y -half_crop : center_y + half_crop, center_x - half_crop : center_x + half_crop])
-
+                img = np.array(get_crop(img, 512 * 2)) # * 2 bc many need to scale by 0.5x and still get a 512px crop
                 # store it in a dict for later (greatly accelerates subsequent epochs)
                 images_cached[item] = img
-                #size = get_size(images_cached)
-                #print(size / (1024. * 1024. * 1024.))
 
-            else:
-                img = images_cached[item]
+            img =  np.array(images_cached[item])
 
-            # to save time use sub_batch_size crops from same image on the same batch
-            # does not really matter images have been cached on images_cached dict.
-            sub_batch_size = args.sub_batch_size
-            assert batch_size % sub_batch_size == 0
-
+            #print("o: ", img.shape, item)
             manipulated = 0.
-            if not inference and np.random.rand() < 0.5:
+            if (np.random.rand() < 0.5) and not inference:
                 img = random_manipulation(img)
+                #print("am: ", img.shape, item)
                 manipulated = 1.
 
+            #print("bc: ", img.shape, item)
+            img = get_crop(img, CROP_SIZE)
+            #print("ac: ", img.shape, item)
             img = preprocess_image(img)
+            #print("ap: ", img.shape, item)
 
-            for sub_batch in range(sub_batch_size):
+            X[batch_idx] = img
+            O[batch_idx] = manipulated
 
-                # right now just take corners of image as crops
-                sx= 0 #random.randint(0, 1)
-                sy= 0 #random.randint(0, 1)
-
-                _sx = img.shape[1] - CROP_SIZE if sx == 1 else 0
-                _sy = img.shape[0] - CROP_SIZE if sy == 1 else 0
-
-                _img = img[_sy:_sy+CROP_SIZE, _sx:_sx+CROP_SIZE]
-
-                X[batch_idx+sub_batch] = _img
-                O[batch_idx+sub_batch] = manipulated - 0.5 # between -0.5 an 0.5
-                y[batch_idx+sub_batch] = class_idx
+            if not inference:
+                y[batch_idx] = class_idx
 
             if batch_idx == 0 and False: #remove False if you want to see images on stdout (requires iterm2)
                 show_image(X[batch_idx])
 
-            batch_idx += sub_batch_size
+            batch_idx += 1
 
             if batch_idx == batch_size:
 
@@ -290,41 +298,9 @@ class CameraImagesSequence(Sequence):
             img = load_img(item)
 
             # resize image so it's same as test images
-            img = skimage.transform.resize(img, (512, 512), mode='reflect')
+            #img = skimage.transform.resize(img, (512, 512), mode='reflect')
 
-            if args.kernel_filter:
-                # see slide 13
-                # http://www.lirmm.fr/~chaumont/publications/WIFS-2016_TUAMA_COMBY_CHAUMONT_Camera_Model_Identification_With_CNN_slides.pdf
-                kernel_filter = 1/12. * np.array([\
-                    [-1,  2,  -2,  2, -1],  \
-                    [ 2, -6,   8, -6,  2],  \
-                    [-2,  8, -12,  8, -2],  \
-                    [ 2, -6,   8, -6,  2],  \
-                    [-1,  2,  -2,  2, -1]]) 
-
-                img = cv2.filter2D(img.astype(np.float32),-1,kernel_filter)
-                # kernel filter already puts mean ~0 and roughly scales between [-1..1]
-                # no need to preprocess_input further
-            else:
-                # find `preprocess_input` function specific to the classifier
-                classifier_to_module = { 
-                    'NASNetLarge'       : 'nasnet',
-                    'NASNetMobile'      : 'nasnet',
-                    'DenseNet121'       : 'densenet',
-                    'DenseNet161'       : 'densenet',
-                    'DenseNet201'       : 'densenet',
-                    'InceptionResNetV2' : 'inception_resnet_v2',
-                    'InceptionV3'       : 'inception_v3',
-                    'MobileNet'         : 'mobilenet',
-                    'ResNet50'          : 'resnet50',
-                    'VGG16'             : 'vgg16',
-                    'VGG19'             : 'vgg19',
-                    'Xception'          : 'xception',
-                }
-
-                classifier_module_name = classifier_to_module[args.classifier]
-                preprocess_input_function = getattr(globals()[classifier_module_name], 'preprocess_input')
-                img = preprocess_input_function(img.astype(np.float32))
+            img = preprocess_image(img)
 
             # at this point we now have img
             sx= random.randint(0, 1)
@@ -352,36 +328,60 @@ class CameraImagesSequence(Sequence):
                 random.shuffle(I_O_zipped)
                 self.X_path, self.y = zip(*I_O_zipped)
 
+
+
+def SmallNet(include_top, weights, input_shape, pooling):
+    img_input = Input(shape=input_shape)
+    x = Conv2D( 3, (7, 7), strides=(1,1), padding='valid', name='filtering')(img_input)
+
+    x = Conv2D(64, (3, 3), strides=(2,2), padding='valid', name='conv1')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(64, (3, 3), strides=(2,2), padding='valid', name='conv2')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(32, (3, 3), strides=(1,1), padding='valid', name='conv3')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2,2), padding='valid')(x)
+
+    model = Model(img_input, x, name='smallnet')
+
+    return model
+
 # Custom layer used to do filtering on the GPU
 # Not sure if it yields correct results so not using right now.
 class KernelFilter(Layer):
 
-        def __init__(self, **kwargs):
-                super(KernelFilter, self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super(KernelFilter, self).__init__(**kwargs)
 
-        def build(self, input_shape):
+    def build(self, input_shape):
                 
-                self.kernel_initializer = Constant(1/12. * np.float32([\
-                    [-1,  2,  -2,  2, -1],  \
-                    [ 2, -6,   8, -6,  2],  \
-                    [-2,  8, -12,  8, -2],  \
-                    [ 2, -6,   8, -6,  2],  \
-                    [-1,  2,  -2,  2, -1] ]))
+        self.kernel_initializer = Constant(1/12. * np.float32([\
+            [-1,  2,  -2,  2, -1],  \
+            [ 2, -6,   8, -6,  2],  \
+            [-2,  8, -12,  8, -2],  \
+            [ 2, -6,   8, -6,  2],  \
+            [-1,  2,  -2,  2, -1] ]))
 
-                self.kernel = self.add_weight(
-                    name='kernel',
-                    shape=(5,5,3,3), # need to check this!
-                    initializer=self.kernel_initializer,
-                    trainable=False)
+        self.kernel = self.add_weight(
+            name='kernel',
+            shape=(5,5,3,3), # need to check this!
+            initializer=self.kernel_initializer,
+            trainable=False)
 
-                super(KernelFilter, self).build(input_shape)  # Be sure to call this somewhere!
+        super(KernelFilter, self).build(input_shape)  # Be sure to call this somewhere!
 
-        def call(self, x):
+    def call(self, x):
 
-                print(x.shape)
-                print(self.kernel)
+        print(x.shape)
+        print(self.kernel)
 
-                return K.conv2d(x, self.kernel, padding='same', data_format='channels_last')
+        return K.conv2d(x, self.kernel, padding='same', data_format='channels_last')
 
 if args.model:
     print("Loading model " + args.model)
@@ -389,7 +389,7 @@ if args.model:
     model = load_model(args.model, compile=False)
     match = re.search(r'([A-Za-z_\d\.]+)-epoch(\d+)-.*\.hdf5', args.model)
     model_name = match.group(1)
-    last_epoch = int(match.group(2)) + 1
+    last_epoch = int(match.group(2))
 else:
     last_epoch = 0
 
@@ -405,31 +405,33 @@ else:
         input_shape=(CROP_SIZE, CROP_SIZE, 3), 
         pooling=args.pooling if args.pooling != 'none' else None)
 
-    x = classifier_model(image_filtered)
+    x = Conv2D(3, (7, 7), strides=(1,1), use_bias=False, padding='valid', name='filtering')(image_filtered)
+    x = classifier_model(x)
     if args.pooling == 'none':
         x = Flatten()(x)
     x = Dropout(args.dropout)(x)
     x = concatenate([x,manipulated])
-    x = Dense(512, activation='relu')(x)
+    x = Dense(256, activation='relu')(x)
     x = Dropout(args.dropout)(x)
     x = Dense(128,  activation='relu')(x)
+    x = Dropout(args.dropout)(x)
     prediction = Dense(N_CLASSES, activation ="softmax", name="predictions")(x)
 
     model = Model(inputs=(input_image, manipulated), outputs=prediction)
-
-    model.summary()
-
     model_name = args.classifier + ('_kf' if args.kernel_filter else '') + '_do' + str(args.dropout) + '_' + args.pooling
 
+model.summary()
 model = multi_gpu_model(model, gpus=args.gpus)
 
-if not args.test:
+
+
+if not (args.test or args.test_train):
     # TRAINING
 
     ids = glob.glob(join(TRAIN_FOLDER,'*/*.jpg'))
     ids.sort()
 
-    ids_train, ids_val = train_test_split(ids, test_size=0.1, random_state=42)
+    ids_train, ids_val = train_test_split(ids, test_size=0.1, random_state=SEED)
 
     opt = Adam(lr=args.learning_rate)
     #opt = SGD(lr=args.learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
@@ -460,22 +462,46 @@ if not args.test:
                 #max_queue_size=16)
 else:
     # TEST
-    ids = glob.glob(join(TEST_FOLDER,'*.tif'))
+    if args.test:
+        ids = glob.glob(join(TEST_FOLDER,'*.tif'))
+    elif args.test_train:
+        ids = glob.glob(join(TRAIN_FOLDER,'*/*.jpg'))
+    else:
+        assert False
+
     ids.sort()
+    
+    from conditional import conditional
 
-    with open('submission.csv', 'w') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(['fname','camera'])
+    with conditional(args.test, open('submission.csv', 'w')) as csvfile:
 
-        for idx in tqdm(ids):
+        if args.test:
+            csv_writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(['fname','camera'])
+        else:
+            correct_predictions = 0
+
+        for i, idx in enumerate(tqdm(ids)):
 
             img = np.array(Image.open(idx))
+
+            img = get_crop(img, CROP_SIZE)
+            #img = np.transpose(img, axes=(1,0,2))
             img = np.expand_dims(preprocess_image(img), axis=0)
 
-            manipulated = np.float32([[0.5 if idx.find('manip') != -1 else -0.5]])
+            manipulated = np.float32([[1. if idx.find('manip') != -1 else 0.]])
 
             prediction = model.predict_on_batch([img,manipulated])
-            class_idx = np.argmax(prediction)
+            prediction_class_idx = np.argmax(prediction)
 
-            csv_writer.writerow([idx.split('/')[-1], CLASSES[class_idx]])
+            if args.test_train:
+                class_idx = CLASSES.index(idx.split('/')[-2])
+                if class_idx == prediction_class_idx:
+                    correct_predictions += 1
+
+            if args.test:
+
+                csv_writer.writerow([idx.split('/')[-1], CLASSES[prediction_class_idx]])
+        if args.test_train:
+            print("Accuracy: " + str(correct_predictions / i))
                 
